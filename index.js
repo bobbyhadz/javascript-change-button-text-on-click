c509141c961084514f94a98969ef4440d7f7451e
bobbyhadz <opmftw@gmail.com>
2024-03-05 12:57:54 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log('bobbyhadz.com');
+
+const btn = document.getElementById('btn');
+
+// ✅ Change button text on click
+btn.addEventListener('click', function handleClick() {
+  btn.textContent = 'Button clicked';
+});
+
+/**
+ * ✅ If you need to change the button's inner HTML use:
+ *  - `innerHTML` instead of `textContent`
+ */
